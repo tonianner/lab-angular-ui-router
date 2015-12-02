5a19a96ec9e15a5356b04eba070803a16305df5d
Toni Anne Romero <tonianne.romero@gmail.com>
2015-12-02 15:39:48 +0800

    initial commit

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,1 +1,22 @@
-angular.module('InfamousCriminals', []);
+angular
+  .module('InfamousCriminals', ['ui.router', 'xeditable'])
+  .config(MainRouter);
+
+function MainRouter($stateProvider, $urlRouterProvider){
+
+  $stateProvider
+    .state('list',{
+      url:'/',
+      templateUrl:'list.html'
+    })
+    .state('new',{
+      url:'/new',
+      templateUrl:'new.html'
+    })
+    .state('about',{
+      url:'/about',
+      templateUrl:'about.html'
+    })
+
+  $urlRouterProvider.otherwise('/');
+}
--- frontend/js/criminalsController.js
+++ frontend/js/criminalsController.js
@@ -1,40 +1,47 @@
-angular.module('InfamousCriminals')
+angular
+.module('InfamousCriminals')
+
+.run(function(editableOptions) {
+  editableOptions.theme = 'bs3';
+})
+
 .controller('CriminalsController', CriminalsController);
 
-CriminalsController.$inject = ['$http', '$scope'];
+  CriminalsController.$inject = ['$http', '$scope', '$state'];
 
-function CriminalsController($http, $scope){
-  $scope.all = [];
-  $scope.addCriminal = addCriminal;
-  $scope.newCriminal = {};
-  $scope.getCriminals = getCriminals;
-  $scope.deleteCriminal = deleteCriminal;
-
-  getCriminals();
-  function getCriminals(){
-    $http
-      .get('http://localhost:3000/criminals')
-      .then(function(response){
-        $scope.all = response.data.criminals;
-    });
-  }
-
-  function addCriminal(){
-    $http
-      .post('http://localhost:3000/criminals', $scope.newCriminal)
-      .then(function(response){
-        getCriminals();
-    });
+  function CriminalsController($http, $scope, $state){
+    $scope.all = [];
+    $scope.addCriminal = addCriminal;
     $scope.newCriminal = {};
-  }
+    $scope.getCriminals = getCriminals;
+    $scope.deleteCriminal = deleteCriminal;
 
-  function deleteCriminal(criminal){
-    $http
-      .delete("http://localhost:3000/criminals/" + criminal._id)
-      .then(function(response){
-        var index = $scope.all.indexOf(criminal);
-        $scope.all.splice(index, 1);
+    getCriminals();
+    function getCriminals(){
+      $http
+        .get('http://localhost:3000/criminals')
+        .then(function(response){
+          $scope.all = response.data.criminals;
       });
-  }
+    }
 
-}
+    function addCriminal(){
+      $http
+        .post('http://localhost:3000/criminals', $scope.newCriminal)
+        .then(function(response){
+          getCriminals();
+          $state.go('list');
+      });
+      $scope.newCriminal = {};
+    }
+
+    function deleteCriminal(criminal){
+      $http
+        .delete("http://localhost:3000/criminals/" + criminal._id)
+        .then(function(response){
+          var index = $scope.all.indexOf(criminal);
+          $scope.all.splice(index, 1);
+        });
+    }
+
+  }
